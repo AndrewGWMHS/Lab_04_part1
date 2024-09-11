63a33e17e12bab7532cf2f1371f53216d76b4ae0
AndrewGWMHS <goff.614615@swl.k12.oh.us>
2024-09-11 08:28:36 -0400

    Initial commit

---
+++ src/LabOne.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[] args) {
+        int intOperandA = 9;
+        int intOperandB = 3;
+        int intSum;
+        int intDifference;
+        int intProduct;
+        int intQuotient;
+        int intModulo;
+
+        double doubleOperandA = 9.0;
+        double doubleOperandB = 3.0;
+        double doubleSum;
+        double doubleDifference;
+        double doubleProduct;
+        double doubleQuotient;
+        double doubleModulo;
+
+        intSum = intOperandA + intOperandB;
+        System.out.println("The sum using ints of " + intOperandA + " and " + intOperandB +  " is " + intSum);
+
+    }
+}
